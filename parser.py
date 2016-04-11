4de177d92a79319a9b5747bc8a0aad8b81b71ac0
TusharGupta01 <tushar.behror@gmail.com>
2016-04-11 23:13:30 +0530

    First commit for sentimental analysis

---
+++ parser.py
@@ -0,0 +1,33 @@
+""" 
+FILE : parser.py
+DESCRIPTION : This code is basically made to parse the data which 
+          we have got from the tweeter API. Basically we are extracting 
+          the tweet that a user has made, ignoring all other data.
+
+AUTHOR : Tushar Gupta
+
+BUG FIXES :
+		April 11,2016  
+"""
+
+import sys
+import csv
+import math
+import nltk
+import re
+
+def parser(filename):
+	fp = open(filename, "r")
+	# temp list to store all the tweet
+	tweet = [] 
+	for line in fp:
+		l = re.findall(r'text=u(.*?), is_quote_status',line)
+		tweet.append(l[0])
+		return tweet
+
+# Get file name by command line argument
+filename = sys.argv[1]
+parser(filename)
+
+
+
---
+++ sentimental.py
@@ -0,0 +1,54 @@
+""" 
+FILE : parser.py
+DESCRIPTION : This code is made to do the actual data analysis.
+
+AUTHOR : Tushar Gupta
+
+BUG FIXES :
+		April 11,2016  
+"""
+
+import sys
+import csv
+import math
+import nltk.tokenize import RegexpTokenizer
+import nltk.corpus import stopwords
+import re
+
+# Cleaning of data
+
+class Splitter(object):
+    def __init__(self):
+        self.nltk_splitter = nltk.data.load('tokenizers/punkt/english.pickle')
+        self.nltk_tokenizer = nltk.tokenize.TreebankWordTokenizer()
+
+    def split(self, text):
+        """
+        input format: a paragraph of text
+        output format: a list of lists of words.
+            e.g.: [['this', 'is', 'a', 'sentence'], ['this', 'is', 'another', 'one']]
+        """
+        sentences = self.nltk_splitter.tokenize(text)
+        tokenized_sentences = [self.nltk_tokenizer.tokenize(sent) for sent in sentences]
+        return tokenized_sentences
+
+
+class POSTagger(object):
+    def __init__(self):
+        pass
+        
+    def pos_tag(self, sentences):
+        """
+        input format: list of lists of words
+            e.g.: [['this', 'is', 'a', 'sentence'], ['this', 'is', 'another', 'one']]
+        output format: list of lists of tagged tokens. Each tagged tokens has a
+        form, a lemma, and a list of tags
+            e.g: [[('this', 'this', ['DT']), ('is', 'be', ['VB']), ('a', 'a', ['DT']), ('sentence', 'sentence', ['NN'])],
+                    [('this', 'this', ['DT']), ('is', 'be', ['VB']), ('another', 'another', ['DT']), ('one', 'one', ['CARD'])]]
+        """
+
+        pos = [nltk.pos_tag(sentence) for sentence in sentences]
+        #adapt format
+        pos = [[(word, word, [postag]) for (word, postag) in sentence] for sentence in pos]
+        return pos
+
