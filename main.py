9767dcc617fde3450cfafbbb8459f063dd9ff421
Sagar Ghai <sagarghai18695@gmail.com>
2016-07-19 23:02:53 +0530

    keyword added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-""" 
+"""
 FILE : main.py
-DESCRIPTION : This code is the utility by which user need not to 
+DESCRIPTION : This code is the utility by which user need not to
 	know any deatils of the framework rather use this abstraction.
 
 Run Command: main.py [json file name]
@@ -8,13 +8,14 @@ Run Command: main.py [json file name]
 AUTHOR : Tushar Gupta
 
 BUG FIXES :
-		June 22,2016  
+		June 22,2016
 """
 import sys
 import sentimental
 import nltk
 import Hashtag
 import twitter
+from nltk.corpus import stopwords
 
 try:
     count = sys.argv[2]
@@ -33,14 +34,22 @@ postagger = sentimental.POSTagger()
 dicttagger = sentimental.DictionaryTagger(['positive-words.yml', 'negative-words.yml'])
 filename = (str)(raw_input('Enter the filename to store individual tweet score > '))
 if 'csv' not in filename.split('.'):
-    filename + '.csv'
+    filename = filename + '.csv'
 file2 = open(filename, 'w+')
+filename1 = (str)(raw_input('Enter the filename to store keywords > '))
+file1 = open(filename1, 'w+')
 sum = 0
 for tweet in listoftweets:
-    # print tweet
+    tkt = []
+    stop = stopwords.words('english')
+    for i in tweet.split():
+        if i not in stop:
+            tkt.append(i)
+    tkts = ','.join(tkt)
+    tkts = tkts + '\n'
+    file1.write(tkts)
     splitted_sentences = splitter.split(tweet)
     pos_tagged_sentences = postagger.pos_tag(splitted_sentences)
-    # print(pos_tagged_sentences)
     dict_tagged_sentences = dicttagger.tag(pos_tagged_sentences)
     a = sentimental.sentiment_score(dict_tagged_sentences)
     keywords = sentimental.keyword_token(dict_tagged_sentences)
